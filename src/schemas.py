848be9d8c66cba8d7513037a8456b04ce1adfac1
Vildan <khismatullin.vildan@mail.ru>
2024-06-09 23:14:19 +0500

    add: extended pydantic models

--- src/schemas.py
+++ src/schemas.py
@@ -15,4 +15,39 @@ class TrackCreateModel(BaseModel):
 
 class AlbumCreateModel(BaseModel):
     title: str
+    artist_id: int
     image_file: UploadFile | None = None
+
+
+class AlbumWithTracksCreateModel(AlbumCreateModel):
+    tracks: list[TrackCreateModel]
+
+
+class ArtistModel(BaseModel):
+    id: int
+    name: str
+    image_file_name: str | None = None
+
+
+class AlbumModel(BaseModel):
+    title: str
+    image_file_name: str | None = None
+
+
+class TrackModel(BaseModel):
+    title: str
+    audio_file_name: str
+    image_file_name: str | None = None
+    artists: list[ArtistModel]
+
+
+class AlbumWithTracksModel(AlbumModel):
+    tracks: list[TrackModel]
+
+
+class ArtistWithAlbumsModel(ArtistModel):
+    albums: list[AlbumModel]
+
+
+class ArtistWithAlbumsAndTracksModel(ArtistModel):
+    albums: list[AlbumWithTracksModel]
