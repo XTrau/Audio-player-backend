71bcce64b4c1cf37688fd77145dce0d3e9e599a4
Vildan <khismatullin.vildan@mail.ru>
2024-06-11 14:27:54 +0500

    fix: Redesigned the models

--- src/database.py
+++ src/database.py
@@ -5,12 +5,13 @@ from src.config import settings
 print(settings.DATABASE_URL_asyncpg)
 
 engine = create_async_engine(
-    url=settings.DATABASE_URL_asyncpg
+    url=settings.DATABASE_URL_asyncpg,
+    echo=True
 )
 
 new_session = async_sessionmaker(
     bind=engine,
-    expire_on_commit=False
+    expire_on_commit=False,
 )
 
 
@@ -20,9 +21,9 @@ class Base(DeclarativeBase):
 
 async def create_tables():
     async with engine.begin() as conn:
-        conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
 async def drop_tables():
     async with engine.begin() as conn:
-        conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.drop_all)
--- src/models.py
+++ src/models.py
@@ -3,35 +3,35 @@ from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import ForeignKey
 
 
-class TrackOrm(Base):
-    __tablename__ = 'tracks'
-
-    id: Mapped[int] = mapped_column(primary_key=True, auto_increment=True)
-    title: Mapped[str]
-    audio_file_name: Mapped[str]
-    image_file_name: Mapped[str]
-
-    album: Mapped['AlbumOrm'] = relationship()
-    artists: Mapped[list['ArtistOrm']] = relationship()
-
-
 class ArtistOrm(Base):
     __tablename__ = 'artists'
 
-    id: Mapped[int] = mapped_column(primary_key=True, auto_increment=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
-    image_file_name: Mapped[str]
+    image_file_name: Mapped[str | None]
 
-    albums: Mapped[list['AlbumOrm']] = relationship()
+    albums: Mapped[list["AlbumOrm"]] = relationship(back_populates="artist")
 
 
 class AlbumOrm(Base):
     __tablename__ = 'albums'
 
-    id: Mapped[int] = mapped_column(primary_key=True, auto_increment=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str]
-    artist_id: Mapped[int] = mapped_column(ForeignKey('artists.id'), primary_key=True)
-    image_file_name: Mapped[str]
+    artist_id: Mapped[int] = mapped_column(ForeignKey('artists.id'))
+    image_file_name: Mapped[str | None]
 
-    artist: Mapped['ArtistOrm'] = relationship()
-    tracks: Mapped['TrackOrm'] = relationship()
+    artist: Mapped["ArtistOrm"] = relationship(back_populates="albums", overlaps="albums")
+    tracks: Mapped[list["TrackOrm"]] = relationship(back_populates="album")
+
+
+class TrackOrm(Base):
+    __tablename__ = 'tracks'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    audio_file_name: Mapped[str]
+    image_file_name: Mapped[str | None]
+    album_id: Mapped[int] = mapped_column(ForeignKey('albums.id'))
+
+    album: Mapped["AlbumOrm"] = relationship(back_populates="tracks")
