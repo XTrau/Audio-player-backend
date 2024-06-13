9a954873cc57f2f6dc7d3887485320c745e6c5d5
Vildan <khismatullin.vildan@mail.ru>
2024-06-13 11:25:19 +0500

    add: track router add: CORSMiddleware do requests from localhost

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from src.database import drop_tables, create_tables
 
 from src.artists.router import router as artists_router
-from src.albums.router import router as album_router
+from src.albums.router import router as albums_router
+from src.tracks.router import router as tracks_router
 from src.files.router import router as file_router
 
 app = FastAPI()
-app.include_router(artists_router)
-app.include_router(album_router)
+app.include_router(artists_router, prefix='/api')
+app.include_router(albums_router, prefix='/api')
+app.include_router(tracks_router, prefix='/api')
 app.include_router(file_router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
