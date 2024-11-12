0fffc718cc66cac082c1e8c1eb55d1aeb0ea0ea8
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2024-11-12 16:31:37 +0500

    update artist name validator, add regular expression validation

--- src/artists/schemas.py
+++ src/artists/schemas.py
@@ -1,3 +1,5 @@
+from typing import re
+
 from fastapi import UploadFile, File, HTTPException
 from pydantic import BaseModel, field_validator
 
@@ -7,11 +9,17 @@ class SArtistBase(BaseModel):
 
     @field_validator("name")
     @classmethod
-    def validate_name_length(cls, value: str) -> str:
+    def validate_name(cls, value: str) -> str:
         if not (3 <= len(value) <= 64):
             raise HTTPException(
                 status_code=422,
                 detail="Имя артиста должно быть длиной от 3 до 64 символов.",
+            )
+        regex = r"^[A-Za-zА-Яа-яЁё\s'-$]{3,64}$"
+        if not re.match(regex, value):
+            raise HTTPException(
+                status_code=422,
+                detail="Имя артиста может содержать только буквы русского и английского алфавитов, пробелы, дефисы и апострофы, и быть длиной от 3 до 64 символов."
             )
         return value
 
