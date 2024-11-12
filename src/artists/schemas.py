f52e358a63a24e77b881124a25778b183526faca
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2024-11-12 17:10:11 +0500

    fix re import, remove validator from not create schemas

--- src/artists/schemas.py
+++ src/artists/schemas.py
@@ -1,11 +1,12 @@
-from typing import re
+import re
 
 from fastapi import UploadFile, File, HTTPException
 from pydantic import BaseModel, field_validator
 
 
-class SArtistBase(BaseModel):
+class SArtistCreate(BaseModel):
     name: str
+    image_file: UploadFile | None = File(None)
 
     @field_validator("name")
     @classmethod
@@ -15,19 +16,16 @@ class SArtistBase(BaseModel):
                 status_code=422,
                 detail="Имя артиста должно быть длиной от 3 до 64 символов.",
             )
-        regex = r"^[A-Za-zА-Яа-яЁё\s'-$]{3,64}$"
+        regex = r"^[A-Za-zА-Яа-яЁё0-9\s'$-]{3,64}$"
         if not re.match(regex, value):
             raise HTTPException(
                 status_code=422,
-                detail="Имя артиста может содержать только буквы русского и английского алфавитов, пробелы, дефисы и апострофы, и быть длиной от 3 до 64 символов."
+                detail="Имя артиста может содержать только буквы русского и английского алфавитов, цифры, пробелы, дефисы и апострофы, и быть длиной от 3 до 64 символов.",
             )
         return value
 
 
-class SArtistCreate(SArtistBase):
-    image_file: UploadFile | None = File(None)
-
-
-class SArtist(SArtistBase):
+class SArtist(BaseModel):
     id: int
+    name: str
     image_file_name: str | None = None
